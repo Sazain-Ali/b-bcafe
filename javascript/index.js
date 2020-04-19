2f4ea0537749d709d94ef44a1b703e5f58560f97
Sazain-Ali <sazain25@gmail.com>
2020-04-19 20:30:35 +0500

    updated

--- javascript/index.js
+++ javascript/index.js
@@ -1,75 +1,24 @@
-//alert("hi");//
 
+		//Form Validation//
+     				('button').click(function(){
+	                 var name = $('#name').val();
+	                 console.log(name);
 
-// light gallery//
-			 $(document).ready(function() {
-        $(".lightgallery").lightGallery(); 
-    });
+	                 var email = $('#email').val();
+	                 console.log(email);
 
-
-    //slider home page//
-$('.slideshow').slick({
-     		autoplay:true,
-     		autoplayspeed:500,
-     		  fade: true,
-     		 cssEase: 'linear'
-     		});
-		
-
-     		//form//
-var name="abcdef";
-var email="abc@gmail.com"
-
-$('button').click(function(){
-	var name=$('#name').val();
-	console.log(name);
-
-	var email=$('#email').val();
-	console.log(email);
-
-	//conditions for validation//
-			//name//
-		if (name == "") {
+	                if (name == "") {
 					$('#id').text("Empty!");
-				}
+				    }
 
-				
-				else{
-
-				}
-
-
-					//email//
-		if (email == "") {
+				    if (email == "") {
 					$('#mail').text("Empty!");
-				}
+				    }
 
-				else{
-
-				}
-					//successful//
-				if(name != "" && email != ""){
-
-						$.alert({
-   	 					 title: 'Alert!',
-  					  content: 'successful!',
-						});
-						}
-				else{
-
-				}
-
-
-});
-			
-				
-
-				
-
-
-// discount animation//
-//$('body').hover(function(){
-//	$('#discount').fadeToggle();
-//})
-
-
+				    if(name != "" && email != ""){
+			          $.alert({
+					  title: 'Your form has been submitted!',
+  					  content: 'Thank You for contacting us.We will get back to you.',
+					});
+			      }
+			  })
